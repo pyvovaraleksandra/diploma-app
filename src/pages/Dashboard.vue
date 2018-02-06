3ba5776e2be0a64c1ac06452a34505c086068184
evhena <evhena.bogd@gmail.com>
2018-02-06 19:53:26 +0200

    add 2 section and navigation between

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -15,18 +15,25 @@
                   <img class="rounded-circle profile__content-left-avatar mx-auto" src="../assets/avatar.png" alt="avatar">
                 </div>
                 <div class="row">
-                  <p class="profile__content-left-name col-sm-7 col-md-12 mx-auto">{{name}}</p>
+                  <p class="profile__content-left-name col-sm-7 col-md-12 mx-auto">name</p>
                 </div>
                 <div class="profile__content-left-buttons left-buttons row flex-column">
-                  <div class="col-10 col-sm-7 col-md-12 mx-auto">
-                    <a href="#addTask" class="left-buttons-button left-buttons-button-darkblue">Add Task</a>
+                  
+                  <div v-on:submit.prevent="toAddTask" 
+                       class="col-10 col-sm-7 col-md-12 mx-auto">
+                    <router-link :to="{name: 'AddTask'}" class="left-buttons-button left-buttons-button-darkblue">Add Task </router-link>
                   </div>
-                  <div class="col-10 col-sm-7 col-md-12 mx-auto">
-                    <a href="#profile" class="left-buttons-button left-buttons-button-blue">Edit profile</a>
+
+                  <div v-on:submit.prevent="toProfile"
+                       class="col-10 col-sm-7 col-md-12 mx-auto">
+                    <router-link :to="{name: 'Profile'}" class="left-buttons-button left-buttons-button-blue">Edit profile</router-link>
                   </div>
-                  <div class="col-10 col-sm-7 col-md-12 mx-auto">
-                    <a href="#exit" class="left-buttons-button left-buttons-button-pink">exit</a>
+
+                  <div v-on:submit.prevent="toWelcome"
+                       class="col-10 col-sm-7 col-md-12 mx-auto">
+                    <router-link :to="{name: 'Welcome'}" class="left-buttons-button left-buttons-button-pink">exit</router-link>
                   </div>
+
                 </div>
               </div>
             </div>
@@ -140,8 +147,20 @@
 <script>
   export default {
     name: 'Dashboard',
+    methods: {
+        toAddTask: function() {
+          this.$router.push({ name: 'addTask' });
+        },
+        
+        toProfile: function() {
+          this.$router.push({ name: 'profile' });
+        }, 
 
-  }
+        toWelcome: function() {
+          this.$router.push({ name: 'welcome' });
+        }
+      }
+  };
 </script>
 
 <style lang="scss">
